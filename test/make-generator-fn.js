f46e9368db04e0725a56e2bd0a246ab52123ed35
Jordan Harband <ljharb@gmail.com>
2013-09-12 21:54:23 -0700

    Tests.

---
+++ test/make-generator-fn.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var makeGeneratorFunction = function () {
+    return Function('return function* () { var x = yield 6; return x * 7; }')();
+};
+var generatorFunc;
+try { generatorFunc = makeGeneratorFunction(); } catch (e) {}
+
+module.exports = generatorFunc;
+
