a62e7a547307f5ba62a39e374f2cc2f46705eabc
Jordan Harband <ljharb@gmail.com>
2015-01-31 10:29:56 -0800

    Bail out early when typeof is not "function"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fnToStr = Function.prototype.toString;
 var isFnRegex = /^\s*function\*/;
 
 module.exports = function isGeneratorFunction(fn) {
+	if (typeof fn !== 'function') { return false; }
 	var fnStr = toStr.call(fn);
 	return (fnStr === '[object Function]' || fnStr === '[object GeneratorFunction]') && isFnRegex.test(fnToStr.call(fn));
 };
