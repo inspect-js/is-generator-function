6158b97f161530c28133e6c906c762a2d642fb9b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-08-07 18:35:41 +0800

    simplified the logic

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var toStr = Object.prototype.toString;
-var fnToStr = Function.prototype.toString;
-var isFnRegex = /^\s*(?:function)?\*/;
-
 module.exports = function isGeneratorFunction(fn) {
-	if (typeof fn !== 'function') { return false; }
-	var fnStr = toStr.call(fn);
-	return (fnStr === '[object Function]' || fnStr === '[object GeneratorFunction]') && isFnRegex.test(fnToStr.call(fn));
+	return typeof fn ==='function' && fn.constructor.name==='GeneratorFunction';
 };
