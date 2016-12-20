9d4d7ac23f6f2f75098903b4fe4f74e1d39a2226
Jordan Harband <ljharb@gmail.com>
2016-12-19 21:53:13 -0800

    [Refactor] remove useless `Object#toString` check.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var toStr = Object.prototype.toString;
 var fnToStr = Function.prototype.toString;
 var isFnRegex = /^\s*(?:function)?\*/;
 
@@ -8,6 +7,5 @@ module.exports = function isGeneratorFunction(fn) {
 	if (typeof fn !== 'function') {
 		return false;
 	}
-	var fnStr = toStr.call(fn);
-	return (fnStr === '[object Function]' || fnStr === '[object GeneratorFunction]') && isFnRegex.test(fnToStr.call(fn));
+	return isFnRegex.test(fnToStr.call(fn));
 };
