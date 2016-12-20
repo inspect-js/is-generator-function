3a3a52bdba46e03ae333af9519bf471207bf6cec
Jordan Harband <ljharb@gmail.com>
2016-12-19 23:12:09 -0800

    [Fix] account for Safari 10 which reports the wrong toString on generator functions.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
 
+var toStr = Object.prototype.toString;
 var fnToStr = Function.prototype.toString;
 var isFnRegex = /^\s*(?:function)?\*/;
+var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
+var getProto = Object.getPrototypeOf;
+var GeneratorFunction = hasToStringTag ? getProto(Function('return function*() {}')()) : {};
 
 module.exports = function isGeneratorFunction(fn) {
 	if (typeof fn !== 'function') {
 		return false;
 	}
-	return isFnRegex.test(fnToStr.call(fn));
+	if (isFnRegex.test(fnToStr.call(fn))) {
+		return true;
+	}
+	if (!hasToStringTag) {
+		var str = toStr.call(fn);
+		return str === '[object GeneratorFunction]';
+	}
+	return getProto(fn) === GeneratorFunction;
 };
